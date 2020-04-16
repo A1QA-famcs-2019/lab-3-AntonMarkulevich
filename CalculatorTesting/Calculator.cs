a68afc1755f1e9cc6a69ebf5f82ab10e8600028b
Anton Markulevich <37664391+AntonMarkulevich@users.noreply.github.com>
2020-04-16 11:21:59 +0300

    Init commit

---
+++ CalculatorTesting/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace CalculatorTesting
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Singleton claculator instance.
+        /// </summary>
+        private static Calculator _instance = null;
+
+        /// <summary>
+        /// Get calculator singleton instance.
+        /// </summary>
+        public static Calculator Instance
+        {
+            get { return _instance ?? (_instance = new Calculator()); }
+        }
+
+        /// <summary>
+        /// Calculator process.
+        /// </summary>
+        private Application _application = null;
+
+        /// <summary>
+        /// Empty constructor.
+        /// </summary>
+        private Calculator()
+        {
+        }
+
+        /// <summary>
+        /// Launch calculator process.
+        /// </summary>
+        /// <returns><code>true</code> if process is launched, <code>false</code> otherwise</returns>
+        public void Launch()
+        {
+            _application = _application ?? Application.Launch(ResourceManager.CalculatorApplicationPath);
+        }
+
+        public void Close()
+        {
+            if (_application == null)
+                return;
+
+            _application.Close();
+            _application.Dispose();
+            _application = null;
+            _instance = null;
+        }
+
+        /// <summary>
+        /// Get application visible window
+        /// </summary>
+        /// <returns>Window object if found. null otherwise</returns>
+        public Window GetWindow()
+        {
+            return _application.GetWindow(ResourceManager.CalculatorMainWindowName);
+        }
+
+        /// <summary>
+        /// Destructor. Terminates calculator process.
+        /// </summary>
+        ~Calculator()
+        {
+            Close();
+        }
+    }
+}
+
---
+++ CalculatorTesting/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using CalculatorTesting.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace CalculatorTesting
+{
+    public class MainWindow : BaseWindow
+    {
+        public NumericKeypad KeypadNumeric { get; }
+        public MemoryKeypad KeypadMemory { get; }
+        public Button ButtonAdd { get; }
+        public Button ButtonSub { get; }
+        public Button ButtonMul { get; }
+        public Button ButtonDiv { get; }
+        public Button ButtonGetResult { get; }
+        public Label LabelResult { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="window">Window.</param>
+        public MainWindow(Window window) : base(window)
+        {
+            KeypadNumeric = new NumericKeypad(window);
+            KeypadMemory = new MemoryKeypad(window);
+            ButtonAdd = GetElement<Button>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowButtonAddId));
+            ButtonSub = GetElement<Button>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowButtonSubtractId));
+            ButtonMul = GetElement<Button>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowButtonMultiplyId));
+            ButtonDiv = GetElement<Button>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowButtonDivideId));
+            ButtonGetResult = GetElement<Button>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowButtonGetResultId));
+            LabelResult = GetElement<Label>(SearchCriteria.ByAutomationId(ResourceManager.CalculatorMainWindowLabelResultId));
+        }
+    }
+}
